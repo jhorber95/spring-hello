4aa95257dcbbca036d1b3333875ca342f9bcb363
Jhorkman Bernal <jhorber95@gmail.com>
2025-01-22 14:32:15 -0500

    initial

---
+++ Controller.java
@@ -0,0 +1,13 @@
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api")
+@RestController
+public class Controller {
+
+    @GetMapping
+    public String hello() {
+        return "Hello World!";
+    }
+}
