854b934986e6f5a23a4ee6d24222aaed68799246
Jhorkman Bernal <jhorber95@gmail.com>
2025-01-22 14:42:35 -0500

    commit

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,12 +1,14 @@
+package com.example.demo;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/api")
 @RestController
-public class Controller {
+@RequestMapping("/api")
+public class HelloController {
 
-    @GetMapping
+    @GetMapping("/hi")
     public String hello() {
         return "Hello World!";
     }
